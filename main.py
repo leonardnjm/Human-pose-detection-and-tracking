4237d7be084a9420fa5bdd8e01491a52bafb9c8a
RaphaelBhnk <raphael.bouhnik@edu.ece.fr>
2020-12-11 05:02:08 +0100

    Forme qui suit le mouvement du visage

--- main.py
+++ main.py
@@ -25,9 +25,11 @@ while cap.isOpened():
   # Draw the pose annotation on the image.
   image.flags.writeable = True
   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+
   mp_drawing.draw_landmarks(
       image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-  image = cv2.circle(image, (150, 22), radius=0, color=(255, 0, 255), thickness=100)
+  image = cv2.circle(image, (int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x*200), int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y*100)), radius=0, color=(255, 0, 255), thickness=100)
 
   #print('nose landmark:', results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE])
   #print(results.pose_landmarks.PoseLandmark)
@@ -41,14 +43,9 @@ while cap.isOpened():
   #print(results.pose_landmarks)
   #print(mp_pose.POSE_CONNECTIONS)
 
-
   cv2.imshow('MediaPipe Pose', image)
   
-  print(
-  f'Nose coordinates: ('
-  f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x}, '
-  f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y})'
-  )
+
 
   # if mp_pose.PoseLandmark = 11:
   #   print("yes")
